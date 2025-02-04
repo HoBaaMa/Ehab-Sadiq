796fdf6f287f41c5d2ee043163b9d875a1a687d9
ehab <ehab.sadiq89@gmail.com>
2025-02-04 09:23:22 +0200

    first commit

---
+++ assets/JS/main.js
@@ -0,0 +1,24 @@
+// Add class active to header on scroll
+
+let header = document.querySelector("header")
+
+window.onscroll = function () {
+    if (this.scrollY >= 50) {
+        header.classList.add("active")
+    }
+    else {
+        header.classList.remove("active")
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    var animation = lottie.loadAnimation({
+        container: document.getElementById("lottie-container"),
+        renderer: "svg",
+        loop: true,
+        autoplay: true,
+        path: "web-design-layout.json", // Ensure this file is in the correct path
+    });
+});
+
